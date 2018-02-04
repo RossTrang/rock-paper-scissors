0e0d8ac1597703add94d317e9265efa92fb695e3
Ross <rossrtw2010@gmail.com>
2018-02-04 20:34:40 +0700

    Added game file

---
+++ RPS.js
@@ -0,0 +1,20 @@
+//  https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors
+
+function computerPlay() {
+	// need to generate rock, paper or scissors
+	// use Math.random to generate a number 1-3
+	// use switch cases to return choice
+}
+
+function game() {
+	// play x5 rounds
+	// track scores
+	// indicate overall winner
+
+function playRound(playerChoice, computerChoice) {
+	// use prompt for user input, case insensitive - rock, ROCK, Rock
+	// logic for win permutations
+	// indication of winner
+}
+
+}
